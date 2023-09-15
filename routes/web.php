2934f998a4cedcd9b4753054f67af35758528e3f
Yuviar <yuviarnuzu21@gmail.com>
2023-09-15 12:54:19 +0700

    Ye

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('/welcome');
 });
-Route::resource('/siswa',siswaController::class);
+Route::resource('/siswa', siswaController::class);
